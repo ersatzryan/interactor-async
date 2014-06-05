0f03ec6ade4c9c116bf7d3f714d0a9079cd132cc
Ryan Glover <ersatzryan@gmail.com>
2014-06-05 15:08:59 -0400

    Add async ability

--- interactor-async.gemspec
+++ interactor-async.gemspec
@@ -19,4 +19,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
+
+  spec.add_dependency "interactor", "~> 2.0"
+  spec.add_dependency "sucker_punch", "~> 1.0.5"
 end
--- lib/interactor/async.rb
+++ lib/interactor/async.rb
@@ -1,4 +1,30 @@
 module Interactor
   module Async
+    def self.included(base)
+      base.class_eval do
+        include Interactor
+        include ::Celluloid
+
+        extend ClassMethods
+        extend ::SuckerPunch::Job::ClassMethods
+
+        class << self
+          attr_reader :workers
+        end
+      end
+    end
+
+    def initialize(context = {})
+      self.class.define_celluloid_pool(self.class, self.class.workers)
+      super
+    end
+
+    module ClassMethods
+      def perform(context = {})
+        new(context).tap do |instance|
+          instance.async.perform unless instance.failure?
+        end
+      end
+    end
   end
 end
